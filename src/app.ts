f5524f450dc8ff4078861e89c9d385f36e40c682
Dick Cheung <dick.y.d.cheung@gmail.com>
2021-08-21 19:22:42 +0100

    Init Setup, TS up and running on Nodejs

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response, NextFunction} from "express";
+import bodyParser from "body-parser";
+import _ from "lodash";
+
+import "dotenv/config"
+import Routes from "./Routes";
+import connect from "./connect";
+
+const app: Application = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("TS App is Running, you sure?");
+});
+
+const PORT = process.env.PORT||3000;
+const db = "mongodb://localhost:27017/test"
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`)
+  })
